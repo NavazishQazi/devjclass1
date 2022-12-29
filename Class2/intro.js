b04a9599dd393a7ffbf6e81d545fd622347fe6e1
NavazishQazi <navazishqazi72@gmail.com>
2022-12-29 22:41:09 +0530

    intro to js

---
+++ Class2/intro.js
@@ -0,0 +1,29 @@
+// printing in js
+console.log("hiii welcome to javascript");
+
+// variable in js and data type 
+
+let num =10 //number
+console.log(num);
+
+let char = 'a'; // character
+console.log(char);
+
+let str = 'i am string';//string
+console.log(str);
+
+let bool = true;//boolean
+console.log(bool);
+
+// loops 
+
+for(let i=0; i<5;i++)
+{
+    console.log(i);  
+}
+
+let count = 5;
+while(count>0){
+    console.log(count);
+    count--;
+}
