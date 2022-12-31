83e1fb2b802e919a894a4a561a81427de60c3b6f
NavazishQazi <navazishqazi72@gmail.com>
2022-12-31 11:30:16 +0530

    object in js

---
+++ Class2/object.js
@@ -0,0 +1,14 @@
+// empty object 
+
+let obj ={}
+console.log(obj);
+
+// object 
+
+let objAman = {
+    Name : "Navazish Hussain",
+    lastName : "Qazi",
+    phoneNo : 8963966740,
+    Age : 22
+};
+console.log(objAman);
