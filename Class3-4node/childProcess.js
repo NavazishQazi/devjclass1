a0ffd54bd8d5439dbf7ff091b28f10a44a3fc4d8
NavazishQazi <navazishqazi72@gmail.com>
2022-12-31 21:24:09 +0530

    childprocess in node

---
+++ Class3-4node/childProcess.js
@@ -0,0 +1,12 @@
+// Node - > simple run time enviroment
+// Child Process 
+
+
+let cp = require('child_process');
+console.log(cp);
+
+// open calculator with the help of this file 
+
+cp.execSync("calc");
+cp.execSync('code');
+
