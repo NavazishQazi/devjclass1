a622c4394b88d844402e230d40b4cdb133f73737
NavazishQazi <navazishqazi72@gmail.com>
2023-01-01 15:41:18 +0530

    CRUD operation innode js

---
+++ Class3-4node/fs.js
@@ -0,0 +1,23 @@
+let fs = require("fs");
+// console.log(fs); 
+
+let path = require("path");
+
+let filePath = path.join(__dirname,"file.txt");
+// console.log(filePath);
+
+//it creates file if it does not exists else it override
+// C-CREATE 
+fs.writeFileSync(filePath,"Hello I am a text file");
+
+// R-READ 
+let content = fs.readFileSync(filePath,"utf-8");
+console.log(content);
+
+// U-update 
+fs.appendFileSync(filePath,"\n Newly added content");
+console.log("After update : ")
+console.log(fs.readFileSync(filePath,"utf-8"));
+
+// D-delete 
+fs.unlinkSync(filePath);
