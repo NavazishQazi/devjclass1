8d1f60dce47c2b880ed79b4730a916a7aa902691
NavazishQazi <navazishqazi72@gmail.com>
2023-01-04 17:09:11 +0530

    jsdom basic

--- Web_scrapping/request.js
+++ Web_scrapping/request.js
@@ -1,9 +1,19 @@
 const request = require('request');
+const link= "https://www.espncricinfo.com/series/ipl-2021-1249214/chennai-super-kings-vs-kolkata-knight-riders-final-1254117/full-scorecard"
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-request('http://www.google.com', function (error,body) {
-if(error)
-  console.error('error:', error); // Print the error if one occurred
-else{
-  console.log('body:', body); // Print the HTML for the Google homepage.
+request('link',cb);
+
+function cb(error,html) {
+    if(error)
+    console.error('error:', error); // Print the error if one occurred
+    else{
+    // console.log('body:', html); // Print the HTML for the link page.
+    const dom = new JSDOM(html);
+    console.log(dom.window.document);
+    let teamName = document.querySelectorAll(".ds-w-full ds-bg-fill-content-prime ds-overflow-hidden")
+    console.log(teamName[0].textContent);
+    console.log(teamName[1].textContent);
+    }
 }
-});
