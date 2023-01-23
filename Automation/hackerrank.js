b52aeb385adfc1b8d16279e657414e3b83a9ff94
NavazishQazi <navazishqazi72@gmail.com>
2023-01-24 00:19:05 +0530

    upto problem solving  successful

--- Automation/hackerrank.js
+++ Automation/hackerrank.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const mail = "rivese9513@fom8.com";
 const pass = "123456";
 
-let browserPromise = puppeteer.launch({headless:false});
+let browserPromise = puppeteer.launch({headless:false,defaultViewport: null, args: ['--start-maximized'] });
 let page;
 browserPromise.then(function(browser){
     console.log("Browser is opened");
@@ -14,7 +14,7 @@ browserPromise.then(function(browser){
     let urlpromise = page.goto('https://www.hackerrank.com/access-account/');
 }).then(function(){
     console.log("hacker rank page is opened")
-}).then(function(){ 
+}) .then(function(){ 
     let waitpromise = page.waitForSelector('a.hr_button');
     return waitpromise;
 }).then(function(){
@@ -37,5 +37,44 @@ browserPromise.then(function(browser){
     let loginpromise = page.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
     return loginpromise;
 }).then(function(){
-    console.log("login successful ")
+    console.log("login successful");
+}).then(function(){
+    return waitandclick("[data-automation='algorithms']");
+// .then(function(){
+//     let waitpromise = page.waitForSelector("[data-automation='algorithms']");
+//     return waitpromise;
+// }).then(function(){  
+//     let algorithmsclickPromise = page.click("[data-automation='algorithms']");
+//     return algorithmsclickPromise;
+}).then(function(){
+    let waitPromise = page.waitForSelector(".filter-group");
+    return waitPromise;
+}).then(function(){
+    let DomclickPromise = page.evaluate(function(){
+        let allDivs = document.querySelectorAll(".filter-group");
+        let div = allDivs[3];
+        let clickSelector = div.querySelector(".ui-checklist-list-item input");
+        clickSelector.click();
+        return;
+    })
+    return DomclickPromise;
+}).then(function(){
+    console.log("warmup selected");
+    let waitpromise = page.waitForSelector('.ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled');
+    return waitpromise;
+}).then(function(){
+
 })
+
+
+function waitandclick(selector){
+    return new Promise(function(resolve, reject) {
+        let waitPromise = page.waitForSelector(selector);
+        waitPromise.then(function(){
+            let clickPromise = page.click(selector);
+            return clickPromise;
+        }).then(function(){
+            resolve();
+        });
+    })
+}
