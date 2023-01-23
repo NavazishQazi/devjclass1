42634e5a9f7e66b2c11e48185fea61d5022d82f5
NavazishQazi <navazishqazi72@gmail.com>
2023-01-23 10:40:47 +0530

    call back in js

--- Web_scrapping/callback.js
+++ Web_scrapping/callback.js
@@ -1,19 +1,43 @@
+// const fs = require("fs");
+
+
+// console.log("Before...");
+
+// fs.readFile("file.txt",cb);   //readFileSync is syncronious and readFile is assyncronious.
+// // we use readFile because if the file take to much time to run then readFile function run the next code and when the file is ready to run it do callback the function.
+
+// function cb(error,data){
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log(data+"");
+//     }
+// }
+
+// console.log("After");
+
+// ------------------------------------------------------------------------------
+
+
 const fs = require("fs");
 
 
 console.log("Before...");
 
-fs.readFile("file.txt",cb);   //readFileSync is syncronious and readFile is assyncronious.
-// we use readFile because if the file take to much time to run then readFile function run the next code and when the file is ready to run it do callback the function.
-
-function cb(error,data){
+fs.readFile("file.txt",function (error,data){
     if(error){
         console.log(error);
     }else{
         console.log(data+"");
+
+        fs.readFile("f2.txt",function(error,data){
+            if(error){
+                console.log(error);
+            }else{
+                console.log(data+"");
+            }
+        });
     }
-}
+}); 
 
 console.log("After");
-
-
