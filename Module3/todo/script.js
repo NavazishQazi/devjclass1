62f057667ae5e33b331a66c704d326a67f4d6258
NavazishQazi <navazishqazi72@gmail.com>
2023-01-27 11:58:18 +0530

    TODO Style

--- Module3/todo/script.js
+++ Module3/todo/script.js
@@ -2,12 +2,26 @@ let inputTag = document.querySelector("input");
 let ulTag = document.querySelector("ul");
 
 inputTag.addEventListener('keydown',function(e){
+    // console.log(e);
     let key = e.key;
     if(key== 'Enter'){
         let value = inputTag.value;
+        if(value.length ==0){
+        return
+        } 
         inputTag.value ="";
         let liTag = document.createElement("li");
-        liTag.innerText= value;
+        liTag.innerHTML= `<div>${value}</div>
+        <div class = "delete"><i class= "fa fa-trash"></i></div>`
+        handleRemover(liTag);
         ulTag.appendChild(liTag);
     }
+   
 })
+
+function handleRemover(liTag){
+    let deletediv = liTag.querySelector(".delete");
+    deletediv.addEventListener("click",function(){
+        liTag.remove();
+    })
+}
