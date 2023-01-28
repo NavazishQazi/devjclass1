f9ba6aa5f6c2deb4a5e93806fab4adbbc238c981
NavazishQazi <navazishqazi72@gmail.com>
2023-01-28 13:15:54 +0530

    jira ticket add with priority color

--- Module3/jira/script.js
+++ Module3/jira/script.js
@@ -2,7 +2,9 @@ let addbtn = document.querySelector(".addItem");
 let modalcont = document.querySelector(".modal-cont");
 let taskAreaCont = document.querySelector(".textarea");
 let maincont = document.querySelector(".main-cont");
+let allPriorityColors = document.querySelectorAll(".priority-color");
 let addmodal = true;
+let modalPriorityColor ="black"
 
 
 addbtn.addEventListener("click",function(){
@@ -14,21 +16,32 @@ addbtn.addEventListener("click",function(){
     addmodal=!addmodal;
 })
 
+for(let i=0;i<allPriorityColors.length;i++){
+    let priorityDivOneColor = allPriorityColors[i];
+    priorityDivOneColor.addEventListener("click",function(){
+        for(let j=0;j<allPriorityColors.length;j++){
+            allPriorityColors[j].classList.remove("Active");
+        }
+        priorityDivOneColor.classList.add("Active");
+        modalPriorityColor = priorityDivOneColor.classList[0];
+    })
+}
+
 modalcont.addEventListener("keydown",function(e){
     let key =e.key;
     if(key=='Enter'){
-        createticket(taskAreaCont.value)
+        createticket(modalPriorityColor,taskAreaCont.value)
         taskAreaCont.value="";
         modalcont.style.display ="none";
         addmodal = !addmodal;
     }
 })
 
-function createticket(task){
+function createticket(ticketColor,task){
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute('class','ticket-cont');
     ticketCont.innerHTML=` <div class="ticket-cont">
-                            <div class="ticket-color"></div>
+                            <div class="ticket-color ${ticketColor}"></div>
                             <div class="ticket-id">#qzi110</div>
                             <div class="task-area">${task}</div></div>`
     maincont.appendChild(ticketCont);
