9c0c7058fae39b7f450b48f0a177d8e0ea2fea55
NavazishQazi <navazishqazi72@gmail.com>
2023-01-31 13:03:11 +0530

    complete Jira APC

---
+++ Assignments/Assignment 2/q3.js
@@ -0,0 +1,18 @@
+
+// Q - Write a function f that returns product of x and y with both of the following function calls
+
+// f(x, y)
+// f(x)(y)
+
+function f(x,y){
+    if(y==undefined){
+        return function (y){
+            return x*y;
+        }
+    }else{
+        return x*y;
+    }
+}
+console.log(f(5,6));
+console.log(f(5)(5));
+
